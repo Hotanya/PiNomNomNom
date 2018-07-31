7963151cf9ab35c7bff36805b2fdc84a66d5d587
Hotanya <hotanya.r@gmail.com>
2018-07-31 16:31:57 +1200

    added infinite loop, fixed syntax error

--- nomnom.py
+++ nomnom.py
@@ -7,12 +7,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
-    if GPIO.input(channel);
+    if GPIO.input(channel):
         print("hello world") #do something
     
     else:
-        pass #do nothing
+        print("hello again")
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300) #is pin HIGH or LOW
 GPIO.add_event_callback(channel, callback) #assign function to GPIO pin
 
+#keeps the program running (infinite loop)
+while True:
+    time.sleep(1)
